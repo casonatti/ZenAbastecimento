62636198069cae6a69e286ebbd6332ef75f4c863
casonatti <jeison.casonatti@gmail.com>
2022-11-13 18:09:16 -0300

    added the correct functionality of the new input

--- app/src/main/java/com/example/zenabastecimento/MainActivity.kt
+++ app/src/main/java/com/example/zenabastecimento/MainActivity.kt
@@ -1,10 +1,19 @@
 package com.example.zenabastecimento
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.renderscript.ScriptGroup.Input
+import android.text.Editable
+import android.text.TextWatcher
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
+import androidx.core.widget.doOnTextChanged
+import java.io.File
+import java.io.FileOutputStream
+import java.io.InputStream
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,12 +24,64 @@ class MainActivity : AppCompatActivity() {
         val valorKm: EditText = findViewById<EditText>(R.id.txtNumberKm);
         val valorLitros: EditText = findViewById<EditText>(R.id.txtNumberLitros);
         val btnAdd: Button = findViewById<Button>(R.id.btn_add);
+        var km: Double = 0.0;
+        var litros: Double = 0.0;
+        var result: Double = 0.0;
+
+        val mediaTotalPath = "media_total.txt";
+        val mediaTotalFile = File(mediaTotalPath);
+
+        if(mediaTotalFile.length() > 0) {
+            val lines: List<String> = mediaTotalFile.readLines();
+            var subtotal: Double = 0.0;
+            lines.forEach() {
+                line-> subtotal += line.toDouble();
+            }
+
+            valorMediaTotal.text = (subtotal/lines.size).toString();
+
+        } else {
+            valorMediaTotal.text = "0";
+        }
+
+        valorKm.addTextChangedListener(object : TextWatcher {
+
+            override fun afterTextChanged(s: Editable) {}
+
+            override fun beforeTextChanged(s: CharSequence, start: Int,
+                                           count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence, start: Int,
+                                       before: Int, count: Int) {
+                km = valorKm.text.toString().toDouble();
+            }
+        })
+
+        valorLitros.addTextChangedListener(object : TextWatcher {
+
+            override fun afterTextChanged(s: Editable) {}
+
+            override fun beforeTextChanged(s: CharSequence, start: Int,
+                                           count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence, start: Int,
+                                       before: Int, count: Int) {
+
+                var litrosStr: String?;
+                litrosStr = valorLitros.text.toString()
+                if(litrosStr.isNotEmpty()) {
+                    litros = litrosStr.toDouble();
+                }
+
+                result = km / litros;
+                valorMediaRelativa.text = result.toString();
+            }
+        })
 
         btnAdd.setOnClickListener() {
-            val km = valorKm.text.toString().toDouble()
-            val litros = valorLitros.text.toString().toDouble();
-            val result = km / litros;
-            valorMediaRelativa.text = result.toString();
+            //mediaTotalFile.writeText("Teste!");
         }
     }
 }
